923c741baa98f0d3d6743c2a5728b81024c9f18e
Mike Snare <mikesnare@gmail.com>
2018-08-01 16:56:09 -0400

    Trying to reduce the cost of the overhead for adding images.

--- js/all.js
+++ js/all.js
@@ -1,27 +1,26 @@
 function appendImages(arr) {
-  var start = new Date().getTime(),
+  var start,
     loaded = 0,
-    container = $('.image-container');
+    $container = $('.image-container');
+
+  function imageLoad() {
+    console.log('loaded');
+    if (++loaded === arr.length) {
+      var time = new Date().getTime() - start;
+      $('.results').text('All images loaded in ' + time + ' ms.');
+      var data = {};
+      data.time = time;
+      parent.postMessage(JSON.stringify(data), '*');
+    }
+  }
+  var $div = $('<div></div>');
   arr.forEach(function (url) {
     var bust = new Date().getTime();
     url = url + (url.indexOf('?') > -1 ? '&' : '?') + 'bust=' + bust;
     var img = $('<img src="' + url + '">');
-    img[0].onload = function () {
-      console.log('loaded');
-      if (++loaded === arr.length) {
-        var time = new Date().getTime() - start;
-        $('.results').text('All images loaded in ' + time + ' ms.');
-        var data = {};
-        // if (window.performance) {
-        //   data.time = window.performance.getEntriesByType('resource')
-        //     .filter(entry => entry.initiatorType === 'img')
-        //     .reduce((sum, entry) => {return sum + entry.duration}, 0)
-        // } else {
-          data.time = time;
-        // }
-        parent.postMessage(JSON.stringify(data), '*');
-      }
-    };
-    container.append(img);
+    img[0].onload = imageLoad;
+    $div.append(img);
   });
+  start = new Date().getTime();
+  $container.append($div);
 }
